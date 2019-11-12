81461e28e12f49bce908fd9fc572b0792919d04b
waxxd <lzhangmoony@163.com>
2019-11-12 17:16:36 +0800

    修改时间

--- src/main/java/com/stdwork_management/config/WebMvcConfig.java
+++ src/main/java/com/stdwork_management/config/WebMvcConfig.java
@@ -4,17 +4,12 @@ import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 import com.stdwork_management.interceptor.AuthorizeInterceptor;
 import com.stdwork_management.utils.PropertiesUtils;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.core.convert.support.GenericConversionService;
-import org.springframework.web.bind.support.ConfigurableWebBindingInitializer;
+import org.springframework.format.FormatterRegistry;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-import org.springframework.web.servlet.mvc.method.annotation.RequestMappingHandlerAdapter;
-
-import javax.annotation.PostConstruct;
 
 /**
  * description:
@@ -27,8 +22,8 @@ import javax.annotation.PostConstruct;
 public class WebMvcConfig implements WebMvcConfigurer {
 
 
-    @Autowired
-    private RequestMappingHandlerAdapter handlerAdapter;
+//    @Autowired
+//    private RequestMappingHandlerAdapter handlerAdapter;
 
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
@@ -51,21 +46,24 @@ public class WebMvcConfig implements WebMvcConfigurer {
                 excludePathPatterns("/swagger-resources/**", "/webjars/**", "/v2/**", "/swagger-ui.html/**");
     }
 
-    @PostConstruct
-    public void initEditableAvlidation() {
+//    @PostConstruct
+//    public void initEditableAvlidation() {
+//        ConfigurableWebBindingInitializer initializer = (ConfigurableWebBindingInitializer)handlerAdapter.getWebBindingInitializer();
+//        if(initializer.getConversionService()!=null) {
+//            GenericConversionService genericConversionService = (GenericConversionService)initializer.getConversionService();
+//            genericConversionService.addConverter(new DateConverterConfig());
+//            genericConversionService.addConverter(new StringToStringConvert());
+//
+//        }
+//    }
 
-        ConfigurableWebBindingInitializer initializer = (ConfigurableWebBindingInitializer)handlerAdapter.getWebBindingInitializer();
-        if(initializer.getConversionService()!=null) {
-            GenericConversionService genericConversionService = (GenericConversionService)initializer.getConversionService();
-
-            genericConversionService.addConverter(new DateConverterConfig());
-            genericConversionService.addConverter(new StringToStringConvert());
-
-        }
-
+    @Override
+    public void addFormatters(FormatterRegistry registry) {
+        registry.addConverter(new DateConverterConfig());
+        registry.addConverter(new StringToStringConvert());
     }
 
-//    @Override
+    //    @Override
 //    public void extendMessageConverters(List<HttpMessageConverter<?>> converters) {
 //        converters.removeIf(converter -> converter instanceof MappingJackson2HttpMessageConverter);
 //        converters.add(gsonHttpMessageConverter());
