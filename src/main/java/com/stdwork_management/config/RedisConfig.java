1d9aea13082f8a9ba8d14138d9abc881529264fb
waxxd <lzhangmoony@163.com>
2019-11-13 10:16:08 +0800

    redis config

---
+++ src/main/java/com/stdwork_management/config/RedisConfig.java
@@ -0,0 +1,34 @@
+package com.stdwork_management.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
+
+/**
+ * description:
+ *
+ * @author waxxd
+ * @version 1.0
+ * @date 2019-09-10
+ **/
+@Configuration
+public class RedisConfig {
+
+
+    @Bean(name = "redisTemplate")
+    public RedisTemplate<String, Object> redisTemplate(RedisConnectionFactory factory) {
+        RedisTemplate<String, Object> template = new RedisTemplate<>();
+        template.setConnectionFactory(factory);
+        template.setKeySerializer(new StringRedisSerializer());
+        template.setValueSerializer(new GenericJackson2JsonRedisSerializer());
+        template.setHashKeySerializer(new GenericJackson2JsonRedisSerializer());
+        template.setHashValueSerializer(new GenericJackson2JsonRedisSerializer());
+        template.afterPropertiesSet();
+        return template;
+    }
+
+
+}
